91b2fb305fe3d2394298f6793115bb9d2561f931
mon <sawmon71293@gmail.com>
2025-08-27 12:41:58 +0630

    typescript error fixed

--- app/editTopic/[id]/page.tsx
+++ app/editTopic/[id]/page.tsx
@@ -1,22 +1,29 @@
-import Editform from '@/components/EditForm'
+import Editform from "@/components/EditForm";
 
 const getTopicById = async (id: string) => {
-    try {
-        const res = await fetch(`http://localhost:3000/api/topics/${id}`);
-        if (!res.ok) {
-            throw new Error("Failed to fetch the topic")
-        }
-        return res.json()
-    } catch (error) {
-        console.log(error)
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch the topic");
     }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
+type PageProps = {
+  id: string;
+};
 
-export default async function editTopic({ params }: { params: { id: string } }) {
-    const { id } = await params
-    const { topic } = await getTopicById(id)
-    return (<div>
-        <Editform topic={topic} />
-    </div>)
+export default async function editTopic({ id }: PageProps) {
+  console.log("Params id is:", id);
+  return "";
+  console.log("id is", id);
+  const { topic } = await getTopicById(id);
+  return (
+    <div>
+      <Editform topic={topic} />
+    </div>
+  );
 }
